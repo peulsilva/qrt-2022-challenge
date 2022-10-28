eeb3a08d8375397e2492bd07035726e5bed413f9
peulsilva <70079633+peulsilva@users.noreply.github.com>
2022-10-28 23:03:45 +0200

    Create metrics directory

--- src/metrics/custom_metric_QRT_22_6BhdSkn.py
+++ src/metrics/custom_metric_QRT_22_6BhdSkn.py
@@ -6,9 +6,13 @@ import numpy as np
 import pandas as pd
 
 
-def transform_submission_to_ypred(df_A_beta, x_test, y_test):
+def transform_submission_to_ypred(A : np.ndarray, 
+                                  beta : np.ndarray, 
+                                  X_test : pd.DataFrame, 
+                                  y_test: pd.DataFrame):
+
     """ Transform submission output (A, beta) into predicted returns S_t."""
-    df_A_beta = df_A_beta.to_numpy()
+    df_A_beta = np.hstack( (np.hstack([A.T, beta.reshape((10, 1))])).T )
     
     A = df_A_beta[:-10].reshape((250, 10))
     beta = df_A_beta[-10:].reshape(10)
@@ -19,8 +23,8 @@ def transform_submission_to_ypred(df_A_beta, x_test, y_test):
     if any(E.unstack() > 1e-6): 
         return None
 
-    x_test = x_test.T
-    y_test = y_test.T
+    x_test = X_test.T
+    y_test = X_test.T
 
     x_test = x_test[y_test.columns]
 
@@ -30,7 +34,8 @@ def transform_submission_to_ypred(df_A_beta, x_test, y_test):
     return y_pred.T
 
 
-def metric(df_y_true, df_y_pred):
+def metric(df_y_true : pd.DataFrame,
+           df_y_pred : pd.DataFrame):
     """ Compute metric. """
     if df_y_pred is None:  # If the y_pred has only zeroes, the metric is set to -1.
         return -1.0
@@ -44,3 +49,4 @@ def metric(df_y_true, df_y_pred):
     mean_overlap = (y_true * y_pred).sum(1).mean()
 
     return mean_overlap
+
